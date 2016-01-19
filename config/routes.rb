43e1f4475b7a2d0bc4a3543ece6d22e8ca0b70db
Kirk Smith <kirks@hotmail.co.uk>
2016-01-19 14:04:30 +0000

    models and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resource :restaurants
-  
+  resources :restaurants
+
   get 'restaurants' => 'restaurants#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
